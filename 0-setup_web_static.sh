7dc45f7a3d82716ad0f8bfe8694023c9f6fc6ccf
aissamboudi <aissamboudi@gmail.com>
2024-07-14 23:19:25 +0100

    update 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
+sudo su
+if which nginx > /dev/null 2>&1; then
+	sudo systemctl stop nginx
+	sudo apt-get remove -y nginx
+	sudo apt-get autoremove -y
+	sudo apt-get purge -y nginx
+	sudo apt-get autoremove -y
+	sudo find / -name 'nginx*' -exec rm -rf {} +
+fi
+
 # Install nginx
 apt-get update
 apt-get install -y nginx
@@ -24,7 +34,7 @@ fi
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to ubuntu
-chown -hR $USER:$USER /data/
+chown -hR "$USER":"$USER" /data/
 
 # Edit nginx config
 sed -i 's/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}/'\
