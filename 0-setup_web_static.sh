60b54b782e90387994f557dae81f2296c4ad81a3
aissamboudi <aissamboudi@gmail.com>
2024-07-15 12:25:55 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-if which nginx > /dev/null 2>&1; then
+if command -v nginx > /dev/null 2>&1; then
 sudo systemctl stop nginx
 sudo apt-get remove -y nginx
 sudo apt-get autoremove -y
@@ -18,18 +18,18 @@ sudo mkdir -p /var/www/html/
 sudo chmod -R 755 /var/www
 sudo sh -c 'echo "Hello World!" > /var/www/html/index.html' > /dev/null 2>&1
 sudo sh -c 'echo "Ceci n'\''est pas une page" > /var/www/html/404.html' > /dev/null 2>&1
-sudo sed -i 's/server_name _;/server_name _;\
-	\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\
-	\n\n\terror_page 404 \/404.html;\
-	\n\tlocation = \/404.html {\
-	\n\t\troot \/var\/www\/html;\
-	\n\t\tinternal;\
-	\n\t}\
-	\n\tlocation \/hbnb_static {\
-	\n\t\talias \/data\/web_static\/current;\
-	\n\t}/' /etc/nginx/sites-available/default
-sudo sed -i 's/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\
-	\n\tadd_header X-Served-By "$HOSTNAME";/' /etc/nginx/nginx.conf
+sudo sed -i 's/server_name _;/server_name _;'\
+	'\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;'\
+	'\n\n\terror_page 404 \/404.html;'\
+	'\n\tlocation = \/404.html {'\
+	'\n\t\troot \/var\/www\/html;'\
+	'\n\t\tinternal;'\
+	'\n\t}'\
+	'\n\tlocation \/hbnb_static {'\
+	'\n\t\talias \/data\/web_static\/current;'\
+	'\n\t}/' /etc/nginx/sites-available/default
+sudo sed -i 's/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;'\
+	'\n\tadd_header X-Served-By "$HOSTNAME";/' /etc/nginx/nginx.conf
 # Create directories and symbolic link
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
