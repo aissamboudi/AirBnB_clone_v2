6bd2e60c59771d73c055b29b676c4fe11751f7ec
aissamboudi <aissamboudi@gmail.com>
2024-07-15 01:21:42 +0100

    super update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,51 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
-
-sudo su
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 if which nginx > /dev/null 2>&1; then
-	sudo systemctl stop nginx
-	sudo apt-get remove -y nginx
-	sudo apt-get autoremove -y
-	sudo apt-get purge -y nginx
-	sudo apt-get autoremove -y
-	sudo find / -name 'nginx*' -exec rm -rf {} +
+sudo systemctl stop nginx
+sudo apt-get remove -y nginx
+sudo apt-get autoremove -y
+sudo apt-get purge -y nginx
+sudo apt-get autoremove -y
+sudo find / -name 'nginx*' -exec rm -rf {} +
 fi
 
 # Install nginx
-apt-get update
-apt-get install -y nginx
-
+sudo pt-get update
+sudo apt-get install -y nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /var/www/html/
+sudo chmod -R 755 /var/www
+sudo sh -c 'echo "Hello World!" > /var/www/html/index.html' > /dev/null 2>&1
+sudo sh -c 'echo "Ceci n'\''est pas une page" > /var/www/html/404.html' > /dev/null 2>&1
+sudo sed -i 's/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\n\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}/' /etc/nginx/sites-available/default
+sudo sed -i 's/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/' /etc/nginx/nginx.conf
 # Create directories and symbolic link
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 # Add html content to index
-echo '<html>
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
 	<head>
 	</head>
 	<body>
 		Holberton School
 	</body>
-</html>' > /data/web_static/releases/test/index.html
+</html>
+EOF
 # Create a symblic link
 if [ -d /data/web_static/current ]
 then
-	rm -rf /data/web_static/current
+sudo rm -rf /data/web_static/current
 fi
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership to ubuntu
-chown -hR "$USER":"$USER" /data/
-
+sudo chown -hR "$USER":"$USER" /data/
 # Edit nginx config
-sed -i 's/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}/'\
-	/etc/nginx/sites-available/default
-ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 # Restart nginx
-service nginx restart
+if sudo systemctl is-active --quiet nginx; then
+sudo systemctl stop nginx
+fi
+sudo service nginx start
